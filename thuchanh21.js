f7d44d5e8eb7022b43a75c34719b0f8854e307e4
nguyenvantu <nguyenvantu9332@gmail.com>
2022-08-08 16:28:00 +0700

    thuchanh2

---
+++ thuchanh21.js
@@ -0,0 +1,9 @@
+inputWidth= prompt("nhập Width");
+inputHeight= prompt("nhập Heigh");
+let width= parseInt(inputWidth);
+let height= parseInt(inputHeight);
+let area= width * height;
+let sum= width + height;
+document.write("area:" +area);
+document.write('<br/>');
+document.write("sum:" +sum);
